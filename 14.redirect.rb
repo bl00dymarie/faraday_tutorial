998b9db4262df0ba7005e44edcad6e78ec62f030
lilaluca <marie@digitalegesellschaft.de>
2016-11-30 15:57:56 +0100

    Change back to 14.redirect.rb

--- 14.redirect.rb
+++ 14.redirect.rb
@@ -3,14 +3,9 @@
 require 'faraday'
 require 'faraday_middleware'
 
-# con = Faraday.new 'http://localhost:4567/oldpage' do |con|
-#     con.use FaradayMiddleware::FollowRedirects, limit: 5
-# end
-#
-# res = con.get
-# puts res.body
+con = Faraday.new 'http://localhost:4567/oldpage' do |con|
+    con.use FaradayMiddleware::FollowRedirects, limit: 5
+end
 
-con = Faraday.get 'http://localhost:4567/oldpage'
-
-puts con.body
-puts con.headers
+res = con.get
+puts res.body
