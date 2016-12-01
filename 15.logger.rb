2b586b7a8fb72f9c802875db40e8e79be6a29a83
lilaluca <marie@digitalegesellschaft.de>
2016-12-01 13:44:32 +0100

    Add comments on 15.logger.rb

--- 15.logger.rb
+++ 15.logger.rb
@@ -10,6 +10,7 @@ class MyLogger
         @logger = Logger.new(STDOUT)
     end
 
+    # Here we create a middleware that implements logging to the console:
     def call(env)
         on_request("request", env)
         @app.call(env).on_complete do
@@ -17,16 +18,24 @@ class MyLogger
         end
     end
 
+    # A middleware must implement the call method. It executes a method for a request and for a response.
+
     private
     def on_request(phase, env)
         @logger.info("#{phase} : #{env.method} - #{env.url}") if env.method and env.url
     end
+
+    # Upon generation of a request, the on_request method is called. The method logs the phase, request method, and URL.
 
     private
     def on_response(phase, env)
         @logger.info("#{phase} : #{env.body}") if env.body
     end
 end
+
+# The MyLogger middleware is added to the stack with the use method.
+# When a connection object executes a request, it creates a shared env hash,
+# wraps the outer middlewares around each inner middleware, and executes the call method.
 
 con = Faraday.new(:url => "http://localhost:4567") do |build|
     build.request :url_encoded
